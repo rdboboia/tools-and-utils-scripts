7617bf4312d09fa0ebda1c5b70fe75582e69fb23
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-01-27 22:53:54 +0100

    Update process-affinity-change-all.bat

--- process-affinity-change-all.bat
+++ process-affinity-change-all.bat
@@ -7,8 +7,16 @@
 ::			+ 0000 0001 (bit mask) = 1 (decimal value for the processor affinity) = first core
 ::			+ 0000 0011 (bit mask) = 3 (decimal value for the processor affinity) = first 2 cores
 :vars
-set affinityBitMask=1
+::last 4 cores on a 16 thread CPU
+::set systemAffinity=61440
+:: All cores on a 16 thread CPU
+set systemAffinity=65535
+set steamAffinity=65520
 
-powershell "ForEach($PROCESS in GET-PROCESS) { $PROCESS.ProcessorAffinity=%affinityBitMask%}"
+powershell "ForEach($PROCESS in GET-PROCESS) { $PROCESS.ProcessorAffinity=%systemAffinity%}"
+
+:: powershell "ForEach($PROCESS in GET-PROCESS steam) { $PROCESS.ProcessorAffinity=%steamAffinity%}"
+:: powershell "ForEach($PROCESS in GET-PROCESS steamservice) { $PROCESS.ProcessorAffinity=%steamAffinity%}"
+::  powershell "ForEach($PROCESS in GET-PROCESS steamwebhelper) { $PROCESS.ProcessorAffinity=%steamAffinity%}"
 
 pause
