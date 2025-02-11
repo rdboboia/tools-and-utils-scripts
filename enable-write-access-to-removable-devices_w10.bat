f438792435966f13184fd489ffe9e485dc74fd5a
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-02-11 22:50:20 +0100

    Create enable-write-access-to-removable-devices_w10.bat

---
+++ enable-write-access-to-removable-devices_w10.bat
@@ -0,0 +1,18 @@
+:::::::::::::::::::::::::::::::::::::::::::
+:: Disable external devices write access ::
+:::::::::::::::::::::::::::::::::::::::::::
+
+:: Disable command echo.
+@echo off
+
+:: Set title ::
+title "Disable USB write access"
+
+:: Possible values:
+:: 		Value 0 - Don't deny writting
+:: 		Value 1 - Deny writting
+
+:: Changes registry to apply changes.
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices\{53f5630d-b6bf-11d0-94f2-00a0c91efb8b} /v Deny_Write /t REG_DWORD /d 0 /f
+
+:: No restart required.
