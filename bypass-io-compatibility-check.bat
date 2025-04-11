c2f2d1373bfea3816c2c256a39f45538f3d2431d
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-04-11 16:26:29 +0200

    Update bypass-io-compatibility-check.bat

--- bypass-io-compatibility-check.bat
+++ bypass-io-compatibility-check.bat
@@ -4,4 +4,9 @@ echo WARNING! This scripts requires admin privileges.
 
 fsutil bypassIo state C:\
 
+echo.
+echo -----------------------------------------------
+echo If a driver error is raised try uninstalling your specific NVME driver.
+echo Seems like the Windows standard NVME driver is required, at least for older NVMEs.
+
 pause
